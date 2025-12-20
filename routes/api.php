5af8d6e85c4f290c77b52f2b4697d6c3b6756c47
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-12-20 06:23:25 -0900

    Change some logics in the AdmissionController.php and api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php 
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdmissionController;
 
-Route::post('/admissions', [AdmissionController::class, 'store']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/admissions', [AdmissionController::class, 'index']);
-    Route::patch('/admissions/{id}/status', [AdmissionController::class, 'updateStatus']);
-}); 
+Route::post('/admissions', [AdmissionController::class, 'store']);
+Route::get('/admissions', [AdmissionController::class, 'index']);
+
+Route::patch('/admissions/{id}/status', [AdmissionController::class, 'updateStatus']);
